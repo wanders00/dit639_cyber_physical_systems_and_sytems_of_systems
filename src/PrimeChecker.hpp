93df7bbf1b1d7c8caf1b415a563d926ea742ae93
Andrii Demchenko <demch.andrew@gmail.com>
2024-03-27 14:45:14 +0100

    #4 docs. Adds comments to the PrimeChecker.hpp file

--- src/PrimeChecker.hpp
+++ src/PrimeChecker.hpp
@@ -1,8 +1,13 @@
+// This is a header file for Prime Checker
 #ifndef PRIMECHECKER
 #define PRIMECHECKER
 #include <cstdint>
+
+// Defining the Prime Checker class
 class PrimeChecker {
    public:
+
+    // Defining the isPrime function that takes a 16 bit number
     bool isPrime(uint16_t n);
 };
 #endif
